828a8e646acc7b0da08cdcbb2fc03d67db69ff5c
HishamM1 <hishammedhat0@gmail.com>
2024-02-23 11:40:37 +0200

    Add database migrations for roles, permissions, permission_role, users, countries, and user_profiles tables

---
+++ database/migrations/2024_02_23_093743_add_profile_fields_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('display_name')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->timestamp('phone_verified_at')->nullable();
+            $table->string('photo')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
