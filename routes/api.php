25ede3a6811fe252806dd8b3c21a0b8dc4b2e633
HishamM1 <hishammedhat0@gmail.com>
2024-02-23 11:41:18 +0200

    Add routes for registration, property listing, and booking

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Owner\PropertyController;
+use App\Http\Controllers\User\BookingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('auth/register', RegisterController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('owner/properties', [PropertyController::class, 'index']);
+    Route::get('user/bookings', [BookingController::class, 'index']);
 });
